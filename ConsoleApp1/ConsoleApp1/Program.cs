657b1c722d35d54ed757f012de0a76585d173429
smoljankin <smoljankin@gmail.com>
2024-07-04 19:46:09 +0300

    Add function Min

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,18 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static int Min(int a, int b)
+        {
+            if (a < b) return a;
+            else return b;
+        }
         static void Main(string[] args)
         {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Min(a, b));
         }
     }
 }
